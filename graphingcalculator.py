ca2447de16952d94c6f8b6f663782b3ab996937f
cacosta07 <christian.acosta@student.horizonhonors.org>
2024-08-12 09:31:29 -0700

    Update graphingcalculator.py

--- graphingcalculator.py
+++ graphingcalculator.py
@@ -12,14 +12,15 @@ import numpy as np
 ############ OPTIONS #############
 
 darkmode = 0 # 0-1
-coordinates = 1 # 0-1
+coordinates = 0 # 0-1
+mark_x = 0 # 0-1
 
 decimals_place = 1 # 0-1
 
 ########z#### CAMERA #############
 
-x_scale = 1
-y_scale = 1
+x_scale = 5
+y_scale = 5
 
 x_offset = 0
 y_offset = 52
@@ -33,9 +34,16 @@ center_x = 1 # 0-1
 
 x_scale = float(x_scale)
 y_scale = float(y_scale*2)
+mark_x = float(mark_x)
 
 def nlprint(a): # easier to type non line print
     sys.stdout.write(str(a))
+
+def fN(num):
+  if num % 1 == 0:
+    return int(num)
+  else:
+    return num
 
 for i in range(x_clamp):
     if center_x == 1: #x coordinate calculation
@@ -50,7 +58,7 @@ for i in range(x_clamp):
         #parabola   # x**2
         #steps      # m.floor(x/4)*3
 
-        expression = x
+        expression = m.sin(x)
         #################################
     except:
         print("Expression invalid.")
@@ -61,17 +69,31 @@ for i in range(x_clamp):
         if round(i) == y_offset: #draw center line
             nlprint("\b▒")
         if y+y_offset < y_clamp: #not clamped
+            if x == 0:
+                if darkmode == 0:
+                    nlprint("▒")
+                else:
+                    nlprint("┉")
+            else:
+                if darkmode == 1:
+                    nlprint("┄")
+                else:
+                    nlprint(" ")
+        else:                   #clampededed
             if darkmode == 0:
                 nlprint("█")
             else:
                 nlprint("┄")
-        else:                   #clampededed
-            if darkmode == 0:
-                nlprint("┄")
-            else:
-                nlprint(" ")
 
     if coordinates == 1: #coordinates enable/disable
-        print("▓▒"+">┄┄┄┄┄<[" + str(round((x)*10**decimals_place)/10**decimals_place) + ", " + str(round((expression)*10**decimals_place)/10**decimals_place) + "]")
+        if x == round(mark_x):
+            print("▓▒"+">┄┄┄┄┄<[" + str(round((x)*10**decimals_place)/10**decimals_place) + ", " + str(round((expression)*10**decimals_place)/10**decimals_place) + "]>┄┄┄<( x =",fN(mark_x),",",fN(y), ")")
+        else:
+            print("▓▒"+">┄┄┄┄┄<[" + str(round((x)*10**decimals_place)/10**decimals_place) + ", " + str(round((expression)*10**decimals_place)/10**decimals_place) + "]")
+
     else:
-        print("▓▒")
+        
+        if x == round(mark_x):
+            print("▓▒", "<---",fN(mark_x),",",fN(y))
+        else:
+            print("▓▒")
